07f36cd56dcc99f4289c5c0c5decb52d8f8e882e
dimisus <dimisus@coinweb.io>
2023-12-18 17:07:07 +0100

    chore: upgrade with-react deps chore: setup wasm support plugin

--- packages/with-react/vite.config.ts
+++ packages/with-react/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: { target: "esnext" },
+  plugins: [react(), wasm()],
   server: {
     port: 3000,
   },
