412dd7a561d8597bb43ad4f23dc902bd4a1a1bf2
dimisus <dimisus@coinweb.io>
2023-12-18 17:09:08 +0100

    chore: clean up with-react example feat: add form with simple claim validation functionality

--- packages/with-react/src/App.tsx
+++ packages/with-react/src/App.tsx
@@ -1,8 +1,57 @@
-import reactLogo from "./assets/react.svg";
-import * as helloWorldCm from "hello-world.cm";
 import "./App.css";
+import React, { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+import * as helloWorldCm from "hello-world.cm";
+import { IssuedClaim, useContractClaims } from "./hooks";
+
+interface FormState {
+  keyFirstPart: number | string;
+  keySecondPart: number | string;
+  claimBody: string | number | any;
+}
 
 function App() {
+  const [claimForm, setClaimForm] = useState<FormState>();
+
+  const { fetchClaims, validateClaim, claim, isValid, isLoading } =
+    useContractClaims();
+
+  useEffect(() => {
+    fetchClaims();
+  }, []);
+
+  useEffect(() => {
+    if (claim) {
+      setClaimForm({
+        keyFirstPart: claim.content?.key?.first_part,
+        keySecondPart: claim.content?.key?.second_part,
+        claimBody: claim.content?.body,
+      });
+    }
+  }, [claim]);
+
+  const onClaimFieldChangeHandler =
+    (field: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { value } = e.target;
+      setClaimForm((state) => {
+        if (state) {
+          return { ...state, [field]: Number(value) || value };
+        }
+      });
+    };
+
+  const onSubmitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (claim) {
+      const claimClone = JSON.parse(JSON.stringify(claim)) as IssuedClaim;
+      claimClone.content.key.first_part = claimForm?.keyFirstPart ?? "";
+      claimClone.content.key.second_part = claimForm?.keySecondPart ?? "";
+      claimClone.content.body = claimForm?.claimBody;
+      validateClaim(claimClone);
+    }
+  };
+
   return (
     <>
       <div>
@@ -10,9 +59,53 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Coinweb + React</h1>
+
+      <h1>
+        <a href="https://coinweb.io" target="_blank" className="coinweb">
+          Coinweb
+        </a>{" "}
+        + React
+      </h1>
+
       <h2>Hello World</h2>
-      <h2>{helloWorldCm.contractId}</h2>
+      <p>ContractID</p>
+      <p>{helloWorldCm.contractId}</p>
+
+      <br />
+
+      {isLoading ? (
+        <div className="loader" />
+      ) : (
+        claimForm && (
+          <div className="claim">
+            <form className="claim-form" onSubmit={onSubmitHandler}>
+              <input
+                name="keyFirstPart"
+                value={claimForm.keyFirstPart}
+                onChange={onClaimFieldChangeHandler("keyFirstPart")}
+              />
+              <input
+                name="keySecondPart"
+                value={claimForm.keySecondPart}
+                onChange={onClaimFieldChangeHandler("keySecondPart")}
+              />
+              <input
+                name="claimBody"
+                value={claimForm.claimBody}
+                onChange={onClaimFieldChangeHandler("claimBody")}
+              />
+              <button type="submit">Validate claim</button>
+              <span
+                className={`valid-indicator ${
+                  isValid !== undefined && (isValid ? "valid" : "invalid")
+                }`}
+              >
+                {isValid ? "✅ Claim is valid" : "❌ Claim is invalid"}
+              </span>
+            </form>
+          </div>
+        )
+      )}
     </>
   );
 }
---
+++ packages/with-react/src/hooks.ts
@@ -0,0 +1,59 @@
+import { useRef, useState } from "react";
+import * as CwebWallet from "@coinweb/wallet-lib";
+import { NetworkName } from "@coinweb/wallet-lib/enums";
+import * as helloWorldCm from "hello-world.cm";
+
+const DEV_COINWEB_ENDPOINT = "https://api-devnet.coinweb.io/wallet";
+
+const cwebWalletNode = CwebWallet.connect_to_node(DEV_COINWEB_ENDPOINT);
+
+export type IssuedClaim = CwebWallet.GqlIssuedClaim & {
+  content: {
+    key: {
+      first_part: number | string;
+      second_part: number | string;
+    };
+  };
+};
+
+export const useContractClaims = () => {
+  const claims = useRef<IssuedClaim[]>();
+  const [isValid, setIsValid] = useState<boolean>();
+  const [isLoadingClaims, setIsLoadingClaims] = useState(false);
+
+  const fetchClaims = async () => {
+    const { claimFilter: helloWorldClaimFilter } = helloWorldCm;
+
+    const claimFilters = [helloWorldClaimFilter];
+    const networkToClaimFrom = NetworkName.DEVNET_L1A;
+    const loadAllPages = true;
+
+    try {
+      setIsLoadingClaims(true);
+
+      claims.current = (await CwebWallet.fetch_claims(
+        cwebWalletNode,
+        claimFilters,
+        networkToClaimFrom,
+        loadAllPages
+      )) as IssuedClaim[];
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoadingClaims(false);
+    }
+  };
+
+  const validateClaim = (claim: CwebWallet.GqlIssuedClaim): void => {
+    const isClaimValid = helloWorldCm.isClaimOk(claim);
+    setIsValid(isClaimValid);
+  };
+
+  return {
+    fetchClaims,
+    validateClaim,
+    claim: claims.current?.[0],
+    isValid,
+    isLoading: isLoadingClaims,
+  };
+};
